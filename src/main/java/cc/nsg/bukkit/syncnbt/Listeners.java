ce63d76630968f9d2c02b54b72d9497d1c685a40
Stefan Berggren <nsg@nsg.cc>
2014-07-27 17:57:02 +0200

    Moved all PowerNBT methods to NBTData.

--- src/main/java/cc/nsg/bukkit/syncnbt/Listeners.java
+++ src/main/java/cc/nsg/bukkit/syncnbt/Listeners.java
@@ -1,14 +1,5 @@
 package cc.nsg.bukkit.syncnbt;
 
-import java.sql.Connection;
-import java.sql.PreparedStatement;
-import java.sql.ResultSet;
-import java.sql.SQLException;
-import java.sql.Statement;
-
-import me.dpohvar.powernbt.nbt.NBTContainerItem;
-import me.dpohvar.powernbt.nbt.NBTTagCompound;
-
 import org.bukkit.Material;
 import org.bukkit.scheduler.BukkitRunnable;
 import org.bukkit.entity.Player;
@@ -18,7 +9,6 @@ import org.bukkit.event.player.PlayerJoinEvent;
 import org.bukkit.event.player.PlayerQuitEvent;
 import org.bukkit.inventory.ItemStack;
 import org.bukkit.inventory.PlayerInventory;
-import org.bukkit.material.MaterialData;
 
 /**
  * This class listens for events from Bukkit.
@@ -51,28 +41,28 @@ public class Listeners implements Listener {
     for (int slot = 0; slot < inventory.getSize(); slot++) {
       ItemStack is = inventory.getItem(slot);
       if (is == null) continue;
-      walkInventory(inventory, player, is, slot);
+      plugin.nbt.walkInventory(inventory, player, is, slot);
     }
 
     // Player armor
     ItemStack is = player.getInventory().getHelmet();
     if (is != null) {;
-      walkInventory(inventory, player, is, -100);
+      plugin.nbt.walkInventory(inventory, player, is, -100);
       is.setType(Material.AIR); // clear helmet
     }
     is = player.getInventory().getChestplate();
     if (is != null) {;
-      walkInventory(inventory, player, is, -101);
+      plugin.nbt.walkInventory(inventory, player, is, -101);
       is.setType(Material.AIR); // clear chest
     }
     is = player.getInventory().getLeggings();
     if (is != null) {;
-      walkInventory(inventory, player, is, -102);
+      plugin.nbt.walkInventory(inventory, player, is, -102);
       is.setType(Material.AIR); // clear legs
     }
     is = player.getInventory().getBoots();
     if (is != null) {;
-      walkInventory(inventory, player, is, -103);
+      plugin.nbt.walkInventory(inventory, player, is, -103);
       is.setType(Material.AIR); // clear boots
     }
     
@@ -80,50 +70,9 @@ public class Listeners implements Listener {
     plugin.db.unlockPlayer(player.getName());
   }
   
-  @SuppressWarnings("deprecation")
-  private void walkInventory(PlayerInventory inventory, Player player, ItemStack is, int slot) {
-    plugin.db.openConnection();
-
-    int amount = is.getAmount();
-    short durability = is.getDurability();
-    int type = is.getTypeId();
-    byte data = is.getData().getData();
-    
-    plugin.db.saveItem(slot, player.getName(), amount, durability, type, data);
-    
-    NBTContainerItem con = null;
-    
-    switch (slot) {
-    case -100:
-      con = new NBTContainerItem(inventory.getHelmet());
-      break;
-
-    case -101:
-      con = new NBTContainerItem(inventory.getChestplate());
-      break;
-
-    case -102:
-      con = new NBTContainerItem(inventory.getLeggings());
-      break;
-      
-    case -103:
-      con = new NBTContainerItem(inventory.getBoots());
-      break;
-
-    default:
-      con = new NBTContainerItem(inventory.getItem(slot));
-      break;
-    }
-    
-    NBTTagCompound compound = con.getTag();
-    if (compound == null) return;
-
-    try {
-      plugin.nbt.saveExtraNBTData(compound, slot, -1, player);
-    } catch (SQLException e) {
-      e.printStackTrace();
-    }
-  }
+  /**
+   * A player logs in to the server, race conditions!
+   */
   
   @EventHandler
   public void playerLogin(PlayerJoinEvent event) {
@@ -141,92 +90,11 @@ public class Listeners implements Listener {
             }
           }
           
-          restoreInventory(player);
+          plugin.nbt.restoreInventory(player);
           player.sendMessage("Your items are restored!");
       }
 
     }.runTaskLater(plugin, 40);
   }
-  
-  @SuppressWarnings("deprecation")
-  private void restoreInventory(Player player) {  
-    plugin.db.openConnection();
-
-    PlayerInventory inventory = player.getInventory();
-    Connection connection = plugin.db.getConnection();
-
-    try {
-      String sql = "SELECT * FROM syncnbt_items WHERE player_name = ?";
-      PreparedStatement statement = connection.prepareStatement(sql, Statement.RETURN_GENERATED_KEYS);
-      statement.setString(1, player.getName());
-      ResultSet res = statement.executeQuery();
-      while (res.next()) {
-        int amount = res.getInt("amount");
-        short durability = res.getShort("durability");
-        int type = res.getInt("type");
-        byte data = res.getByte("data");
-        int slot = res.getInt("slot");
-        
-        ItemStack item = new ItemStack(type, amount, durability);
-        item.setData(new MaterialData(data));
-        
-        NBTContainerItem con = null;
-        
-        switch (slot) {
-        case -100:
-          inventory.setHelmet(item);
-          con = new NBTContainerItem(player.getInventory().getHelmet());
-          break;
-
-        case -101:
-          inventory.setChestplate(item);
-          con = new NBTContainerItem(player.getInventory().getChestplate());
-          break;
-
-        case -102:
-          inventory.setLeggings(item);
-          con = new NBTContainerItem(player.getInventory().getLeggings());
-          break;
-          
-        case -103:
-          inventory.setBoots(item);
-          con = new NBTContainerItem(player.getInventory().getBoots());
-          break;
-
-        default:
-          inventory.setItem(slot, item);
-          con = new NBTContainerItem(player.getInventory().getItem(slot));
-          break;
-        }
-        
-        NBTTagCompound rc = plugin.nbt.restoreExtraNBTTags(slot, -1, player);
-        if (rc.size() > 0) {
-          con.setTag(rc);
-        }
-        
-      }
-    } catch (SQLException e) {
-      e.printStackTrace();
-    }
     
-    try {
-      String sql = "DELETE FROM syncnbt_items WHERE player_name = ?";
-      PreparedStatement statement = connection.prepareStatement(sql, Statement.RETURN_GENERATED_KEYS);
-      statement.setString(1, player.getName());
-      statement.execute();
-    } catch (SQLException e1) {
-      e1.printStackTrace();
-    }
-
-    try {
-      String sql = "DELETE FROM syncnbt_nbtdata WHERE player_name = ?";
-      PreparedStatement statement = connection.prepareStatement(sql, Statement.RETURN_GENERATED_KEYS);
-      statement.setString(1, player.getName());
-      statement.execute();
-    } catch (SQLException e1) {
-      e1.printStackTrace();
-    }
-    
-  }
-  
 }
--- src/main/java/cc/nsg/bukkit/syncnbt/NBTData.java
+++ src/main/java/cc/nsg/bukkit/syncnbt/NBTData.java
@@ -7,6 +7,7 @@ import java.sql.SQLException;
 import java.sql.Statement;
 
 import me.dpohvar.powernbt.nbt.NBTBase;
+import me.dpohvar.powernbt.nbt.NBTContainerItem;
 import me.dpohvar.powernbt.nbt.NBTTagByte;
 import me.dpohvar.powernbt.nbt.NBTTagByteArray;
 import me.dpohvar.powernbt.nbt.NBTTagCompound;
@@ -20,15 +21,26 @@ import me.dpohvar.powernbt.nbt.NBTTagShort;
 import me.dpohvar.powernbt.nbt.NBTTagString;
 
 import org.bukkit.entity.Player;
+import org.bukkit.inventory.ItemStack;
+import org.bukkit.inventory.PlayerInventory;
+import org.bukkit.material.MaterialData;
+
+/**
+ * This is a helper class to save/restore NBT data with PowerNBT.
+ * @author Stefan Berggren
+ *
+ */
 
 public class NBTData {
   
   Connection connection = null;
   Database db = null;
+  NBTData nbt = null;
   
-  public NBTData(Database db) {
+  public NBTData(Database db, NBTData nbt) {
     this.connection = db.getConnection();
     this.db = db;
+    this.nbt = nbt;
   }
   
   /**
@@ -225,4 +237,132 @@ public class NBTData {
     
     return compound;
   }
+  
+  @SuppressWarnings("deprecation")
+  public void walkInventory(PlayerInventory inventory, Player player, ItemStack is, int slot) {
+    db.openConnection();
+
+    int amount = is.getAmount();
+    short durability = is.getDurability();
+    int type = is.getTypeId();
+    byte data = is.getData().getData();
+    
+    db.saveItem(slot, player.getName(), amount, durability, type, data);
+    
+    NBTContainerItem con = null;
+    
+    switch (slot) {
+    case -100:
+      con = new NBTContainerItem(inventory.getHelmet());
+      break;
+
+    case -101:
+      con = new NBTContainerItem(inventory.getChestplate());
+      break;
+
+    case -102:
+      con = new NBTContainerItem(inventory.getLeggings());
+      break;
+      
+    case -103:
+      con = new NBTContainerItem(inventory.getBoots());
+      break;
+
+    default:
+      con = new NBTContainerItem(inventory.getItem(slot));
+      break;
+    }
+    
+    NBTTagCompound compound = con.getTag();
+    if (compound == null) return;
+
+    try {
+      nbt.saveExtraNBTData(compound, slot, -1, player);
+    } catch (SQLException e) {
+      e.printStackTrace();
+    }
+  }
+  
+  @SuppressWarnings("deprecation")
+  public void restoreInventory(Player player) {  
+    db.openConnection();
+
+    PlayerInventory inventory = player.getInventory();
+    Connection connection = db.getConnection();
+
+    try {
+      String sql = "SELECT * FROM syncnbt_items WHERE player_name = ?";
+      PreparedStatement statement = connection.prepareStatement(sql, Statement.RETURN_GENERATED_KEYS);
+      statement.setString(1, player.getName());
+      ResultSet res = statement.executeQuery();
+      while (res.next()) {
+        int amount = res.getInt("amount");
+        short durability = res.getShort("durability");
+        int type = res.getInt("type");
+        byte data = res.getByte("data");
+        int slot = res.getInt("slot");
+        
+        ItemStack item = new ItemStack(type, amount, durability);
+        item.setData(new MaterialData(data));
+        
+        NBTContainerItem con = null;
+        
+        switch (slot) {
+        case -100:
+          inventory.setHelmet(item);
+          con = new NBTContainerItem(player.getInventory().getHelmet());
+          break;
+
+        case -101:
+          inventory.setChestplate(item);
+          con = new NBTContainerItem(player.getInventory().getChestplate());
+          break;
+
+        case -102:
+          inventory.setLeggings(item);
+          con = new NBTContainerItem(player.getInventory().getLeggings());
+          break;
+          
+        case -103:
+          inventory.setBoots(item);
+          con = new NBTContainerItem(player.getInventory().getBoots());
+          break;
+
+        default:
+          inventory.setItem(slot, item);
+          con = new NBTContainerItem(player.getInventory().getItem(slot));
+          break;
+        }
+        
+        NBTTagCompound rc = nbt.restoreExtraNBTTags(slot, -1, player);
+        if (rc.size() > 0) {
+          con.setTag(rc);
+        }
+        
+      }
+    } catch (SQLException e) {
+      e.printStackTrace();
+    }
+    
+    try {
+      String sql = "DELETE FROM syncnbt_items WHERE player_name = ?";
+      PreparedStatement statement = connection.prepareStatement(sql, Statement.RETURN_GENERATED_KEYS);
+      statement.setString(1, player.getName());
+      statement.execute();
+    } catch (SQLException e1) {
+      e1.printStackTrace();
+    }
+
+    try {
+      String sql = "DELETE FROM syncnbt_nbtdata WHERE player_name = ?";
+      PreparedStatement statement = connection.prepareStatement(sql, Statement.RETURN_GENERATED_KEYS);
+      statement.setString(1, player.getName());
+      statement.execute();
+    } catch (SQLException e1) {
+      e1.printStackTrace();
+    }
+    
+  }
+
+  
 }
